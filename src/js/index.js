bc04195a8b94d81d2d68a29a95763320b6ec91cb
Kvezac <Kvezak@gmail.com>
2022-12-20 05:57:10 +0300

    Fixit index.js. Implementation of functions according to the task todo.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 /* index.js */
-import { createTodo, addTodo, countTodos, exportTitles, firstTodoTitle, lastTodoTitle } from "./todo.js";
+import {
+  createTodo,
+  addTodo,
+  countTodos,
+  exportTitles,
+  firstTodoTitle,
+  lastTodoTitle,
+} from "./todo.js";
 
 let form = document.querySelector("#todo-list-form");
 let title = document.querySelector("#todo-title");
@@ -12,21 +19,26 @@ let todos = [];
 function render(todos) {
   list.innerHTML = `<h3>Todos (${countTodos(todos)})</h3>`;
   todos.forEach(function (todo) {
-    list.insertAdjacentHTML("beforeend", `<li><div class="card">${todo.icon} &nbsp;<strong>${todo.title}</strong> - by ${todo.author.firstName} ${todo.author.lastName}</strong> in ${todo.category}</div></li>`);
+    list.insertAdjacentHTML(
+      "beforeend",
+      `<li><div class="card">${todo.icon} &nbsp;<strong>${todo.todoTitle}</strong> - by ${todo.user.first} ${todo.user.last}</strong> in ${todo.category}</div></li>`
+    );
   });
 }
 
-form.addEventListener("submit", event => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
+  let gift = completed.checked ? "✅" : "⏳";
   let author = {
     first: "Sam",
-    last: "Blue"
+    last: "Blue",
   };
   let values = {
+    icon: gift,
     todoTitle: title.value,
     completed: completed.checked,
     chosenCategory: category.value,
-    user: author
+    user: author,
   };
   let todo = createTodo(values);
   todos = addTodo(todos, todo);
--- src/js/todo.js
+++ src/js/todo.js
@@ -6,7 +6,24 @@
  * @param {object} data
  */
 export function createTodo(data) {
-	// your code
+  const newTodo = {};
+  const entries = Object.entries(data);
+  while (entries.length) {
+    const [key, value] = entries.pop();
+    if (value != null && typeof value == "object") {
+      for (let deepKey in value) {
+        const newValue = data[key][deepKey];
+        if (newTodo[key]) {
+          newTodo[key][deepKey] = newValue;
+        } else {
+          newTodo[key] = { [deepKey]: newValue };
+        }
+      }
+    } else {
+      newTodo[key] = value;
+    }
+  }
+  return newTodo;
 }
 
 /**
@@ -14,33 +31,42 @@ export function createTodo(data) {
  * @param {Todo} todo
  */
 export function addTodo(todos, todo) {
-	// your code
+  todos.push(todo);
+  return todos;
 }
 
 /**
  * @param {Todo[]} todos
  */
 export function countTodos(todos) {
-	// your code
+  const total = todos.length;
+  return total;
 }
 
 /**
  * @param {Todo[]} todos
  */
 export function firstTodoTitle(todos) {
-	// your code
+  const firstTodos = todos[0].todoTitle;
+  return firstTodos;
 }
 
 /**
  * @param {Todo[]} todos
  */
 export function lastTodoTitle(todos) {
-	// your code
+  const lastTodos = todos[todos.length - 1].todoTitle;
+  return lastTodos;
 }
 
 /**
  * @param {Todo[]} todos
  */
 export function exportTitles(todos) {
-	// your code
+  console.log("export");
+  const titles = todos.map(function (todo) {
+    console.log(todo.todoTitle);
+    return todo.todoTitle;
+  });
+  return titles;
 }
